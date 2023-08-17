a4e242e47c49079752106849d60cf25e804629dd
Jalay Shukla <112970017+JalayShukla123@users.noreply.github.com>
2023-08-17 18:26:09 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    string fileName;
+    string line;
+    int lineNumber = 0;
+    int counter = 0;
+    cout << "Enter the file name: ";
+    getline(cin, fileName);
+    ifstream inputFile(fileName);
+
+    while (getline(inputFile,line)) {//the if loop should be in the while loop because the while loop won't stop without a break statement.
+        if (counter < 24) {
+            lineNumber++;
+            counter++;
+            cout << lineNumber << ". " << line << endl;
+        }
+        else {
+            cout << "Would you like to print the next 24 lines? (1 for yes and anything else for no)\n";
+            int ans = 0;
+            cin >> ans;
+            if (ans == 1) {
+                counter = 0;
+            }
+            else {
+                cout << "The file has been closed.\n";
+                inputFile.close();
+                return 0;
+            }
+        }
+    }
+        return 0;
+    }
